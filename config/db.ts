7f506ff8aa95c8dadaf8dfc001f9a371fd9b9d48
Alex Scarlett <alex.scarlett91@gmail.com>
2020-05-02 03:15:55 -0400

    Structure for data storage makes more sense with MongoDB. Switch from SQL to MongoDB

--- config/db.ts
+++ config/db.ts
@@ -1,87 +1,18 @@
-import { createConnection, createPool } from 'mysql';
+import mongoose from 'mongoose';
 
-/*
-  
-  TODO: Fix this file
+export const connectDB = async (): Promise<void> => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
 
-  TODO: Finish exported methods for working with SQL db outside of this module
-
-*/
-
-const state = {
-  pool: null,
-  mode: null,
-};
-
-exports.connect = (mode, done) => {
-  state.pool = createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-
-  state.mode = mode;
-  done();
-});
-
-exports.get = () => {
-  return state.pool;
-};
-
-exports.fixtures = async (data) => {
-  const pool = state.pool
-  if (!pool) {
-    return done(new Error('Missing database connection.'))
-  }
-
-  const names = Object.keys(data.tables)
-  
-  // TODO: fix rest of this
-};
-
-
-
-/*
-
-    THE FOLLOWING WORKS WHEN IN `server.ts`
-
-    ====================================================================================
-
-
-
-const connection = createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  insecureAuth: true,
-});
-
-connection.connect((err) => {
-  if (err) {
     // tslint:disable-next-line:no-console
-    console.log(`Error: ${err.sqlMessage}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    // tslint:disable-next-line:no-console
+    console.log(`Error: ${err.message}`);
     process.exit(1);
   }
-
-  // tslint:disable-next-line:no-console
-  console.log(`MySQL Connected: ${process.env.DB_NAME}`);
-});
-
-connection.query(
-  'CREATE TABLE user(id int primary key, name varchar(255), age int, address text)',
-  (err) => {
-    if (err) {
-      // tslint:disable-next-line:no-console
-      console.log(err);
-    }
-
-    // tslint:disable-next-line:no-console
-    console.log('Successfully added table');
-  }
-);
-
-connection.end();
-
-*/
+};
