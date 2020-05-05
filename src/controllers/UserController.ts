947df58ce5d5aa9f70bbc394f9605522f9dc5dde
Alex Scarlett <alex.scarlett91@gmail.com>
2020-05-05 16:33:51 -0400

    Add method to UserController to retrieve a user while populating the 'portfolios' array with portfolio data

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -19,11 +19,11 @@ export class UserController implements ControllerBase {
     this.router = Router();
 
     this.router.route('/').get(this.allUsers);
-
     this.router.route('/:id').get(this.getUser);
+    this.router.route('/portfolio/:id').post(this.getUserWithPortfolioData);
   }
 
-  // method to get all documents from the users collection
+  // get all documents from the users collection
   private allUsers = async (req: Request, res: Response): Promise<Response<any>> => {
     try {
       const users = await this.model.find();
@@ -35,12 +35,12 @@ export class UserController implements ControllerBase {
     } catch (err) {
       return res.status(500).json({
         success: false,
-        error: 'Server Error',
+        error: err.message,
       });
     }
   };
 
-  // method to retrieve specific document by ID from users collection
+  // retrieve specific user by ID
   private getUser = async (
     req: Request,
     res: Response,
@@ -63,7 +63,36 @@ export class UserController implements ControllerBase {
     } catch (err) {
       return res.status(500).json({
         success: false,
-        error: 'Server Error',
+        error: err.message,
+      });
+    }
+  };
+
+  // retrieve a user with all portfolio data (not just _id field)
+  private getUserWithPortfolioData = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<any>> => {
+    try {
+      const { id } = req.params;
+      const user = await this.model.findById(id).populate('portfolios');
+
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          error: 'No user found',
+        });
+      }
+
+      return res.status(200).json({
+        success: true,
+        data: user,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        error: err.message,
       });
     }
   };
