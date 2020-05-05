4308b7f844f958bf5ed8f9d51e5de2d917c8cf08
Alex Scarlett <alex.scarlett91@gmail.com>
2020-05-04 23:14:11 -0400

    Fix function for authenticating tokens. Add as middleware to router in any controller that would need auth.

--- src/middlewares/authenticateToken.ts
+++ src/middlewares/authenticateToken.ts
@@ -1,37 +1,33 @@
-import { Request, Response, NextFunction } from 'express';
-import jwt from 'jsonwebtoken';
+import jwt, { VerifyErrors } from 'jsonwebtoken';
+import { Response, Request, NextFunction } from 'express';
 
-/*
-
-    TODO: Need to fix the following function for authenticating tokens
-      - Pass as middleware to requests requiring auth
-
-*/
-
+// token authentication middleware
 export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
   // gather jwt access token from req header
   const authHeader = req.headers.authorization;
+  // split auth header to remove 'Bearer' string and just have token
   const token = authHeader && authHeader.split(' ')[1];
 
-  if (token == null) {
-    return res.status(401).json({
-      auth: false,
-      message: 'No token provided',
+  if (token !== null) {
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET_KEY,
+      (err: VerifyErrors | null, decoded: object | undefined) => {
+        if (err) {
+          return res.json({
+            success: false,
+            message: 'Auth token is not valid',
+          });
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      }
+    );
+  } else {
+    return res.json({
+      successs: false,
+      message: 'No auth token provided',
     });
   }
-
-  jwt.verify(token, process.env.JWT_SECRET_KEY, (err: any, user: any) => {
-    if (err) {
-      return res.status(403).json({
-        auth: false,
-        message: 'Token not verified',
-      });
-    }
-
-    // TODO: 'user' should be property of req object when passed as express middleware, figure out why this is incorrect
-    // user = req.user;
-
-    // pass the execution off to whatever req the client intended
-    next();
-  });
 };
